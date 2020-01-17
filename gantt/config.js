e6649a54aa96fa4e122d578680efa605ec37475e
Jose Manuel Casillas Martin <chemanuel_95@hotmail.com>
2020-01-17 14:45:54 -0600

    Add gantt link to GitHub issues

---
+++ gantt/config.js
@@ -0,0 +1,44 @@
+var git = {
+  repo: 'jmcasimar/ECOLISTICO-showroom',
+  username: 'jmcasimar',
+  password: 'predatoR15'
+};
+
+module.exports = {
+   server: {
+      port: 3001
+   },
+
+   github: git,
+
+   gantt: {
+      // Don't plan anything on saturdays (6) and sundays (0)
+      weekDaysOff: [0,6],
+
+      // Associate each github user to a specifc color on the Gantt chart (default
+      // colors available: ganttBlue, ganttRed, ganttGreen, ganttOrange)
+      //
+      // You can add your additional color definitions into public/stylesheets/style.css
+      //
+      // Beware that the github usernames are case sensitive.
+      colorByDev: {
+         "gh-username": "ganttBlue",
+         "unassigned": "ganttRed"
+      },
+
+      // Configure off periods
+      holidays: {
+         "gh-username": [
+            { start: "2017-08-08", end: "2017-08-13", title: "Summer holidays"}
+         ]
+      },
+
+      // Exclude milestones listed below from the Gantt Chart
+      excludedMilestones: [
+         "Milestone Name"
+      ],
+
+      // Default duration (in days) for an issue which is not labelled
+      defaultDuration: 1
+   }
+};
