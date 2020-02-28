5f0b8e899200803a72c986d335603d031c1e0090
jmcasimar <chemanuel_95@hotmail.com>
2020-02-28 10:03:02 -0600

    Scripting terminado para pantalla principal. Queda pendiente la lógica para hacer el timeLapse en todas las pantallas

--- pages/pantalla_principal/contenedor/js/main.js
+++ pages/pantalla_principal/contenedor/js/main.js
@@ -48,6 +48,9 @@ var Screen = Parse.Object.extend("Screens");
 var query2 = new Parse.Query(Screen);
 let screenId = '';
 
+// Screen object.
+var scr = new Screen();
+
 query1.equalTo('systemId', sysID);
 query1.descending('createdAt');
 query1.limit(1);
@@ -79,8 +82,13 @@ query1.find()
   query2.equalTo('system', ptr);
   query2.find().then((screen) => {
     screenId = screen[0].id;
-    // Update the object.
-    var scr = new Screen();
+
+    const currentDate = new Date();
+    const timeLapse = {
+      render: false,
+      start: currentDate,
+      end: currentDate
+    };
     scr.set('objectId', screenId);
     scr.set('floor1', false);
     scr.set('floor2', false);
@@ -88,8 +96,10 @@ query1.find()
     scr.set('floor4', false);
     scr.set('recirculation', false);
     scr.set('compressor', false);
+    scr.set('growy', false);
+    scr.set('timeLapse', timeLapse);
     scr.save().then((screen) => {
-      console.log('Reset all screen parameters');
+      console.log('Reset all screen parameters', scr.attributes);
     }, (error) => {
       console.log(error);
     });
@@ -171,38 +181,35 @@ let subscription1 = liveConfig.subscribe().then( (sub1) => {
 $(document).ready(function(e) {
     $('img[usemap]').rwdImageMaps();
     $('area').on('click', function() {
-        if ($(this).attr('alt') === 'Puerta de contenedor') {
+        if ($(this).attr('alt') === 'Puerta de contenedor' && scr.attributes.floor1 === false) {
           // Update the object.
-          var scr = new Screen();
           scr.set('objectId', screenId);
           scr.set('floor1', true);
           scr.set('floor2', true);
           scr.set('floor3', true);
           scr.set('floor4', true);
           scr.save().then((screen) => {
-            console.log('Updating state to show system floors');
+            console.log('Updating state to show system floors', scr.attributes);
           }, (error) => {
             console.log(error);
           });
         }
-        else if ($(this).attr('alt') === 'Sistema de Compresión') {
+        else if ($(this).attr('alt') === 'Sistema de Compresión' && scr.attributes.compressor === false) {
           // Update the object.
-          var scr = new Screen();
           scr.set('objectId', screenId);
           scr.set('compressor', true);
           scr.save().then((screen) => {
-            console.log('Updating state to show system compressor system');
+            console.log('Updating state to show system compressor system', scr.attributes);
           }, (error) => {
             console.log(error);
           });
         }
-        else if ($(this).attr('alt') === 'Sistema de Recirculación') {
+        else if ($(this).attr('alt') === 'Sistema de Recirculación' && scr.attributes.recirculation === false) {
           // Update the object.
-          var scr = new Screen();
           scr.set('objectId', screenId);
           scr.set('recirculation', true);
           scr.save().then((screen) => {
-            console.log('Updating state to show recirculation system');
+            console.log('Updating state to show recirculation system', scr.attributes);
           }, (error) => {
             console.log(error);
           });
@@ -213,11 +220,75 @@ $(document).ready(function(e) {
     $(function() {
         $('input[name="datetimes"]').daterangepicker({
             timePicker: true,
-            startDate: moment().startOf('hour'),
-            endDate: moment().startOf('hour').add(32, 'hour'),
+            startDate: moment().startOf('hour').subtract(1, 'days'),
+            endDate: moment().startOf('hour'),
             locale: {
                 format: 'DD/MM/YYYY hh:mm A'
             }
         });
     });
+
+    // When TimeLapse Accept
+    $('#modalAceptar').click(
+      function(e) {
+        e.preventDefault();
+        if (scr.attributes.growy === false && scr.attributes.timeLapse.render === false) {
+          const currentDate = new Date();
+          const startDate = $('input[name="datetimes"]').data('daterangepicker').startDate;
+          const endDate = $('input[name="datetimes"]').data('daterangepicker').endDate;
+
+          if (endDate._d <= currentDate) {
+            const timeLapse = {
+              render: true,
+              start: startDate._d,
+              end: endDate._d
+            };
+            // Update the object.
+            scr.set('objectId', screenId);
+            if (scr.attributes.floor1 === false) { scr.set('floor1', true); }
+            if (scr.attributes.floor2 === false) { scr.set('floor2', true); }
+            if (scr.attributes.floor3 === false) { scr.set('floor3', true); }
+            if (scr.attributes.floor4 === false) { scr.set('floor4', true); }
+            if (scr.attributes.recirculation === false) { scr.set('recirculation', true); }
+            if (scr.attributes.compressor === false) { scr.set('compressor', true); }
+            scr.set('timeLapse', timeLapse);
+            scr.save().then((screen) => {
+              console.log('Updating screen class to start timeLapse', scr.attributes);
+            }, (error) => {
+              console.log(error);
+            });
+          } else {
+            alert('Error: Por favor, seleccione una fecha menor al momento actual.');
+          }
+        } else if (scr.attributes.growy) {
+          alert('Error: No se puede iniciar el timeLapse mientras la secuencia de fotos está corriendo.');
+        }
+      }
+    );
+
+    // When Fotos Grower
+    $('#btnGrower').click(
+      function(e) {
+        e.preventDefault();
+        if (scr.attributes.growy === false && scr.attributes.timeLapse.render === false) {
+          // Update the object.
+          scr.set('objectId', screenId);
+          scr.set('growy', true);
+          if (scr.attributes.floor1 === false) { scr.set('floor1', true); }
+          if (scr.attributes.floor2 === false) { scr.set('floor2', true); }
+          if (scr.attributes.floor3 === false) { scr.set('floor3', true); }
+          if (scr.attributes.floor4 === false) { scr.set('floor4', true); }
+          if (scr.attributes.recirculation === false) { scr.set('recirculation', true); }
+          if (scr.attributes.compressor === false) { scr.set('compressor', true); }
+          scr.save().then((screen) => {
+            console.log('Updating screen class to start growy sequence', scr.attributes);
+          }, (error) => {
+            console.log(error);
+          });
+        } else if (scr.attributes.timeLapse.render){
+          alert('Error: No se puede iniciar la secuencia de fotos mientras el timeLapse está corriendo.');
+        }
+      }
+    );
+
 });
