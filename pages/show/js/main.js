68adaabfbf69b2eae105e50e682408837c201e0a
jmcasimar <chemanuel_95@hotmail.com>
2020-04-09 09:58:33 -0500

    Change IP to localhost in main.js

--- pages/show/js/main.js
+++ pages/show/js/main.js
@@ -3425,8 +3425,8 @@ function startGrowy(){
 }
 
 Parse.initialize("123456", "...", "...");
-//Parse.serverURL = 'http://localhost:1337/parse';
-Parse.serverURL = 'http://192.168.1.84:1337/parse';
+Parse.serverURL = 'http://localhost:1337/parse';
+//Parse.serverURL = 'http://192.168.1.84:1337/parse';
 
 // System ID
 const sysID = "15-113-001";
